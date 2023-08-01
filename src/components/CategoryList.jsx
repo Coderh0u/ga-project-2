987ef9efc413514c43f7d79ee7caa2762034ec30
hou <tang_j-hou@hotmail.com>
2023-08-01 11:10:47 +0800

    modal for categoryList works now

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
+import Card from "./Card";
 
 const CategoryList = (props) => {
   const params = useParams();
   const [entriesInCat, setEntriesInCat] = useState();
+  const idRef = useRef(0);
 
   const getEntriesInCat = async () => {
     const res = await fetch(
@@ -22,17 +24,48 @@ const CategoryList = (props) => {
       <h1>{params.item}</h1>
       {entriesInCat && (
         <>
-          {entriesInCat.entries.map((item) => {
+          {entriesInCat.entries.map((item, idx) => {
             return (
               <div className="row">
-                <div className="col-md-3">API: </div>
-                <div className="col-md-6">{item.API}</div>
-                <button className='col-md-3' onClick={props.setModal(true)}>More Info</button>
+                <div className="col-md-9">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <strong>API: </strong>
+                    </div>
+                    <div className="col-md-7">
+                      <strong>{item.API}</strong>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-2">Description: </div>
+                    <div className="col-md-7">{item.Description}</div>
+                  </div>
+                </div>
+                <button
+                  className="col-md-3"
+                  onClick={() => {
+                    props.setModal(true);
+                    idRef.current = idx;
+                  }}
+                >
+                  More Info
+                </button>
               </div>
-              
             );
           })}
         </>
+      )}
+      {props.modal && (
+        <Card
+          api={entriesInCat.entries[idRef.current].API}
+          desc={entriesInCat.entries[idRef.current].Description}
+          auth={entriesInCat.entries[idRef.current].Auth}
+          https={entriesInCat.entries[idRef.current].HTTPS}
+          cors={entriesInCat.entries[idRef.current].Cors}
+          link={entriesInCat.entries[idRef.current].Link}
+          cat={entriesInCat.entries[idRef.current].Category}
+          setModal={props.setModal}
+        ></Card>
       )}
     </>
   );
